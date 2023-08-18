4772b3ff724a72768613de2430a41f8838f3649f
André Tauffer <andretauffer@Andres-MacBook-Pro.local>
2023-08-18 11:50:25 +0200

    Got a circle animated by music

---
+++ sketch-audio.js
@@ -0,0 +1,70 @@
+const canvasSketch = require("canvas-sketch");
+
+const settings = {
+  dimensions: [2048, 2048],
+  animate: true,
+};
+
+let audio;
+let audioContext, audioData, sourceNode, analyserNode;
+let manager;
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = "white";
+    context.fillRect(0, 0, width, height);
+
+    if (!audioContext) return;
+    analyserNode.getFloatFrequencyData(audioData);
+    const avg = getAverage(audioData);
+
+    context.save();
+    context.translate(width * 0.5, height * 0.5);
+    context.lineWidth = 10;
+    context.beginPath();
+    context.arc(0, 0, Math.abs(avg), 0, Math.PI * 2);
+    context.stroke();
+    context.restore();
+  };
+};
+
+const addListeners = () => {
+  window.addEventListener("mouseup", () => {
+    if (!audioContext) createAudio();
+    if (audio.paused) {
+      audio.play();
+      manager.play();
+    } else {
+      audio.pause();
+      manager.pause();
+    }
+  });
+};
+
+const createAudio = () => {
+  audio = document.createElement("audio");
+  audio.src = "audio/Big City Lights by Up in the Air.mp3";
+
+  audioContext = new AudioContext();
+  sourceNode = audioContext.createMediaElementSource(audio);
+  sourceNode.connect(audioContext.destination);
+
+  analyserNode = audioContext.createAnalyser();
+  sourceNode.connect(analyserNode);
+
+  audioData = new Float32Array(analyserNode.frequencyBinCount);
+};
+
+const getAverage = (data) => {
+  let sum = data.reduce((prev, curr) => prev + curr, 0);
+
+  return sum / data.length;
+};
+
+const start = async () => {
+  addListeners();
+  manager = await canvasSketch(sketch, settings);
+  manager.pause();
+};
+
+start();
